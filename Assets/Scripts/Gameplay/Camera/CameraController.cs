de5ff66e94fa5596792cb37f0d7c9a9ebc54aa31
godzzz <g0dzZz1lla@yandex.ru>
2021-06-16 16:09:09 +0300

    Added camera controller & 2 states

---
+++ Assets/Scripts/Gameplay/Camera/CameraController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace NavySpade.Gameplay.Camera
+{
+    using Core;
+
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] Transform target = null;
+        [SerializeField] List<CameraState> states = new List<CameraState>();
+
+        private CameraState currentState;
+        private int currentStateIndex;
+
+        private void Awake()
+        {
+            enabled = true;
+            SetNextState();
+
+            Game.Restarted += SetNextState;
+            Game.Ended += win => SetNextState();
+        }
+
+        private void LateUpdate()
+        {
+            if (target == null)
+                return;
+
+            LookAt(target);
+        }
+
+        public void SetTarget(Transform target)
+        {
+            this.target = target;
+        }
+
+        private void SetNextState()
+        {
+            if (currentState == null)
+            {
+                SetState(0);
+                return;
+            }
+
+            var nextIndex = currentStateIndex < states.Count - 1 ? currentStateIndex + 1 : 0;
+            SetState(nextIndex);
+        }
+
+        private void SetState(int index)
+        {
+            if (index < 0 || index > states.Count - 1)
+                return;
+
+            currentStateIndex = index;
+            currentState = states[currentStateIndex];
+        }
+
+        private void LookAt(Transform target)
+        {
+            transform.LookAt(target);
+
+            if (currentState == null)
+                return;
+
+            var desiredPosition = target.position + currentState.offset;
+            var smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, currentState.smoothSpeed);
+            transform.position = smoothedPosition;
+        }
+    }
+}
---
+++ Assets/Scripts/Gameplay/Camera/CameraState.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace NavySpade.Gameplay.Camera
+{
+    [CreateAssetMenu(fileName = "CameraState", menuName = "Settings/CameraState", order = 51)]
+    public class CameraState : ScriptableObject
+    {
+        public float smoothSpeed = 0.125f;
+        public Vector3 offset = new Vector3(5, 5, 5);
+    }
+}
