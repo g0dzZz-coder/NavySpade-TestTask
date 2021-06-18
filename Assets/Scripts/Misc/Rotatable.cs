df341dea1f4c42d716f76fa3e85ea1eb33bbe323
godzzz <g0dzZz1lla@yandex.ru>
2021-06-18 14:01:09 +0300

    Added utils & misc

---
+++ Assets/Scripts/Misc/Rotatable.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+namespace NavySpade.Misc
+{
+    public class Rotatable : MonoBehaviour
+    {
+        [Range(0f, 100f)]
+        [SerializeField] float duration = 1f;
+        [SerializeField] LoopType loopType = LoopType.Incremental;
+        [SerializeField] Ease ease = Ease.InCubic;
+
+        private void Start()
+        {
+            StartAnimation();
+        }
+
+        //private void FixedUpdate()
+        //{
+        //    transform.Rotate((Vector3.up + Vector3.right) * speed * Time.fixedDeltaTime);
+        //}
+
+        private void OnDestroy()
+        {
+            DOTween.Kill(transform);
+        }
+
+        private void StartAnimation()
+        {
+            transform.DORotate(new Vector3(0, 180, 0), duration, RotateMode.Fast).SetLoops(-1, loopType).SetEase(ease);
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/EnumerableExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NavySpade.Utils
+{
+    public static class EnumerableExtensions
+    {
+        public static T Random<T>(this IEnumerable<T> enumerable)
+        {
+            if (enumerable == null)
+                throw new ArgumentNullException(nameof(enumerable));
+
+            var random = new ThreadSafeRandom();
+            var list = enumerable as IList<T> ?? enumerable.ToList();
+            return list.ElementAt(random.Next(0, list.Count()));
+        }
+    }
+}
